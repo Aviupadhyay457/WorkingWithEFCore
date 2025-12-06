9bbe8432b51fecacb3519e2924a8268905b1d20f
Avinash Upadhyay <avinash.upadhyay@intelegencia.com>
2025-12-06 19:53:08 +0530

    defined Northwind db context class

---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared;
+
+public class  NorthWind:DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Northwind.db");
+        string connection = $"Filename={path}";
+        optionsBuilder.UseSqlite(connection);
+
+    }
+
+}
