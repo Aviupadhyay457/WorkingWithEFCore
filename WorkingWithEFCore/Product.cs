bb099a61f024ab9ba1581578ce2a238391113624
Avinash Upadhyay <avinash.upadhyay@intelegencia.com>
2025-12-08 12:20:22 +0530

    Created product.cs and Category.cs

---
+++ WorkingWithEFCore/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Packt.Shared;
+
+public class Product {
+    public int ProductId { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public string? ProductName { get; set; }
+
+    [Column("UnitPrice", TypeName="money")]
+    public decimal Cost { get; set; }
+
+    [Column("UnitInStock")]
+    public short Stock { get; set; }
+
+    public bool Discontinued { get; set; }
+
+    public int CategoryId { get; set; }
+    public virtual Category Category{ get; set; }
+    
+}
+
